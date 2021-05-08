015e54698dcd8ed6852ea213490fc7e7455992b0
Tom Pearson <tom.pearson@team.bumble.com>
2021-05-09 00:02:08 +0100

    hack

--- Sources/SwiftUICharts/Base/Extensions/CGPoint+Extension.swift
+++ Sources/SwiftUICharts/Base/Extensions/CGPoint+Extension.swift
@@ -30,9 +30,9 @@ extension CGPoint {
         }
         if let min = min, let max = max, min != max {
             if min <= 0 {
-                stepHeight = (frame.size.height - padding) / CGFloat(max - min)
+                stepHeight = (frame.size.height - padding) / CGFloat(max - min) * 100
             } else {
-                stepHeight = (frame.size.height - padding) / CGFloat(max + min)
+                stepHeight = (frame.size.height - padding) / CGFloat(max + min) * 100
             }
         }
 
